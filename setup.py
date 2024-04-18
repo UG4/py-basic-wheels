3b2f6266579e90efc045b5cd987c387c41201b5c
Arne Naegel <anaegel@users.noreply.github.com>
2024-04-18 22:03:26 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from skbuild import setup  # This line replaces 'from setuptools import setup'
 setup(
-    name="ugcore-cpp",
-    version="1.2.3",
+    name="ug4",
+    version="0.0.1",
     description="a minimal example package (cpp version)",
     author='The scikit-build team',
     license="MIT",
     packages=['ugcore'],
-    python_requires=">=3.7",
+    python_requires=">=3.10",
     # cmake_source_dir="ugcore",
     cmake_args=['-DPARALLEL:BOOL=OFF','-DLimex:BOOL=ON', '-DCPU=1', '-DDIM=2']
 )
